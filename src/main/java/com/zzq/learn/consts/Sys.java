454fedd20cb327eb6e3fce72ebe16ffaee013be4
ZZQ <18736271057@163.com>
2021-12-09 16:41:24 +0800

    -

--- src/main/java/com/zzq/learn/consts/Sys.java
+++ src/main/java/com/zzq/learn/consts/Sys.java
@@ -1,5 +1,8 @@
 package com.zzq.learn.consts;
 
+import org.springframework.boot.system.ApplicationHome;
+
 public interface Sys {
+    String JarHome = new ApplicationHome().getDir().toString();
 
 }
