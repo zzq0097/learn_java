cc83b6c81a1e443c085c5b577a9ae8235a4d61ce
ZZQ <18736271057@163.com>
2023-02-13 17:58:07 +0800

    -

---
+++ src/test/java/com/zzq/learn/redis/RedisLockTests.java
@@ -0,0 +1,23 @@
+package com.zzq.learn.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import javax.annotation.Resource;
+import java.time.Duration;
+
+@SpringBootTest
+public class RedisLockTests {
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Test
+    public void lock() {
+        ValueOperations<String, Object> opsForValue = redisTemplate.opsForValue();
+        Boolean getLock = opsForValue.setIfAbsent("lock:test", "1", Duration.ofMinutes(30));
+        System.out.println(getLock);
+    }
+}
