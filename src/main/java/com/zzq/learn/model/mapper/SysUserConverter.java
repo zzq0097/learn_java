8ace7a425f647a0a00e19f51cf240b18570bb93d
ZZQ <18736271057@163.com>
2023-03-31 00:02:39 +0800

    -

--- src/main/java/com/zzq/learn/model/mapper/SysUserConverter.java
+++ src/main/java/com/zzq/learn/model/mapper/SysUserConverter.java
@@ -3,9 +3,10 @@ package com.zzq.learn.model.mapper;
 import com.zzq.learn.model.dto.UserUpdateDTO;
 import com.zzq.learn.model.entity.SysUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper
-public interface SysUserMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface SysUserConverter {
 
     UserUpdateDTO entity2updateDto(SysUser sysUser);
 }
--- src/test/java/com/zzq/learn/mapstruct/SimpleTest.java
+++ src/test/java/com/zzq/learn/mapstruct/SimpleTest.java
@@ -2,20 +2,21 @@ package com.zzq.learn.mapstruct;
 
 import com.zzq.learn.model.dto.UserUpdateDTO;
 import com.zzq.learn.model.entity.SysUser;
-import com.zzq.learn.model.mapper.SysUserMapper;
+import com.zzq.learn.model.mapper.SysUserConverter;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
-import org.mapstruct.factory.Mappers;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SimpleTest {
-    SysUserMapper sysUserMapper = Mappers.getMapper(SysUserMapper.class);
+    @Resource
+    private SysUserConverter sysUserConverter;
 
     @Test
     public void test() {
         SysUser sysUser = new SysUser();
         sysUser.setNickname("nickname");
-        UserUpdateDTO userUpdateDTO = sysUserMapper.entity2updateDto(sysUser);
+        UserUpdateDTO userUpdateDTO = sysUserConverter.entity2updateDto(sysUser);
         System.out.println(userUpdateDTO);
     }
 }
