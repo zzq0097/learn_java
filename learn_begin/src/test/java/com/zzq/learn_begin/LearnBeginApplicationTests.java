c769d008497e1cbbcf21eabb69a5e1c50e7f3b04
ZZQ <18736271057@163.com>
2023-04-08 22:24:47 +0800

    - 更改目录

---
+++ learn_begin/src/test/java/com/zzq/learn_begin/LearnBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzq.learn_begin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnBeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
