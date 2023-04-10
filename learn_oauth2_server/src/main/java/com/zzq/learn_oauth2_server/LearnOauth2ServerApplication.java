381d16fb9440c4d143edcb828a0be537e340a93a
ZZQ <18736271057@163.com>
2023-04-10 22:12:15 +0800

    -

---
+++ learn_oauth2_server/src/main/java/com/zzq/learn_oauth2_server/LearnOauth2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.zzq.learn_oauth2_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauth2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnOauth2ServerApplication.class, args);
+    }
+
+}
--- learn_oauth2_server/src/test/java/com/zzq/learn_oauth2_server/LearnOauth2ServerApplicationTests.java
+++ learn_oauth2_server/src/test/java/com/zzq/learn_oauth2_server/LearnOauth2ServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.zzq.learn_begin;
+package com.zzq.learn_oauth2_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearnBeginApplicationTests {
+class LearnOauth2ServerApplicationTests {
 
     @Test
     void contextLoads() {
