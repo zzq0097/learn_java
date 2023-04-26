200b46cc47c42c0fc01fa189ff66b0f66d253b86
ZZQ <18736271057@163.com>
2023-04-26 14:15:21 +0800

    -

--- learn_nacos_config/src/main/java/com/zzq/learn_nacos_config/LearnNacosConfigApplication.java
+++ learn_nacos_config/src/main/java/com/zzq/learn_nacos_config/LearnNacosConfigApplication.java
@@ -2,9 +2,7 @@ package com.zzq.learn_nacos_config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class LearnNacosConfigApplication {
 
