3a1ade0040a4714c140bb5f6477557ee15f9651a
ZZQ <18736271057@163.com>
2023-11-14 23:20:17 +0800

    - 自定义登录

--- learn_spring_security_simple/src/main/java/com/zzq/learn_spring_security_simple/config/SecurityConfig.java
+++ learn_spring_security_simple/src/main/java/com/zzq/learn_spring_security_simple/config/SecurityConfig.java
@@ -1,27 +1,74 @@
 package com.zzq.learn_spring_security_simple.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zzq.learn_spring_security_simple.model.result.R;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 public class SecurityConfig {
 
     private final static String[] allowed = new String[]{
-            "/login"
+            "/anon"
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry
-                .requestMatchers(allowed).permitAll()
-                .anyRequest().authenticated());
-
-        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
-
+                        .requestMatchers(allowed).permitAll()
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(configurer -> {
+                    configurer.successHandler((request, response, authentication) -> {
+                        writeJson(200, R.ok("token"), response);
+                    });
+                    configurer.failureHandler((request, response, exception) -> {
+                        writeJson(401, R.fail("认证失败"), response);
+                    });
+                })
+                .authenticationManager(this::doAuthentication)
+                .exceptionHandling(configurer -> {
+                    configurer.accessDeniedHandler((request, response, accessDeniedException) -> {
+                        writeJson(403, R.fail("无权限"), response);
+                    });
+                    configurer.authenticationEntryPoint((request, response, authException) -> {
+                        writeJson(401, R.fail("未登录"), response);
+                    });
+                });
         return httpSecurity.build();
-
     }
+
+    public Authentication doAuthentication(Authentication authentication) {
+        // 从数据库查询
+        String username = "admin";
+        String password = "123456";
+        String[] authorities = new String[]{"admin"};
+
+        if (username.equals(authentication.getName()) && password.equals(authentication.getCredentials())) {
+            return new UsernamePasswordAuthenticationToken(username, password, AuthorityUtils.createAuthorityList(authorities));
+        }
+        throw new BadCredentialsException("用户名或密码错误");
+    }
+
+    public void writeJson(int status, R<?> r, HttpServletResponse response) {
+        response.setStatus(status);
+        response.addHeader("Content-Type", "application/json;charset=utf-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            response.getWriter().print(objectMapper.writeValueAsString(r));
+        } catch (Exception e) {
+            log.error("响应写入失败", e);
+        }
+    }
+
 }
--- learn_spring_security_simple/src/main/java/com/zzq/learn_spring_security_simple/controller/LoginController.java
+++ learn_spring_security_simple/src/main/java/com/zzq/learn_spring_security_simple/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.zzq.learn_spring_security_simple.controller;
 
 import com.zzq.learn_spring_security_simple.model.result.R;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +12,9 @@ public class LoginController {
         return R.ok("hello");
     }
 
-    @PostMapping("login")
-    public R<?> login() {
-        return R.fail();
+    @GetMapping("anon")
+    public R<?> anon() {
+        return R.ok("anon");
     }
 
 
